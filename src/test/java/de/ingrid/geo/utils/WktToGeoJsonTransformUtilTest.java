fe1f86905a9698190991792f352b7b286ceb1589
André Wallat <andre.wallat@wemove.com>
2022-01-10 12:10:51 +0100

    Adapt tests

--- src/test/java/de/ingrid/geo/utils/WktToGeoJsonTransformUtilTest.java
+++ src/test/java/de/ingrid/geo/utils/WktToGeoJsonTransformUtilTest.java
@@ -76,7 +76,7 @@ public class WktToGeoJsonTransformUtilTest extends TestCase {
         String wkt = "POINT(30 10)";
         String geojson = WktToGeoJsonTransformUtil.wktToGeoJsonTransform(wkt, "4326", "3857");
 
-        String expected = "{\"type\":\"Point\",\"coordinates\":[3339584.7238,1118889.9749]}";
+        String expected = "{\"type\":\"Point\",\"coordinates\":[3339584.723798207,1118889.9748579597]}";
         assertEquals(expected, geojson);
     }
 
@@ -84,9 +84,9 @@ public class WktToGeoJsonTransformUtilTest extends TestCase {
        String wkt = "GEOMETRYCOLLECTION (POINT (40 10), LINESTRING (10 10, 20 20, 10 40), POLYGON ((40 40, 20 45, 45 30, 40 40)))";
        String geojson = WktToGeoJsonTransformUtil.wktToGeoJsonTransform(wkt, "4326", "3857");
 
-       String expected = "{\"type\":\"GeometryCollection\",\"geometries\":[{\"type\":\"Point\",\"coordinates\":[4452779.6317,1118889.9749]},"
-               + "{\"type\":\"LineString\",\"coordinates\":[[1113194.9079,1118889.9749],[2226389.8159,2273030.927],[1113194.9079,4865942.2795]]},"
-               + "{\"type\":\"Polygon\",\"coordinates\":[[[4452779.6317,4865942.2795],[2226389.8159,5621521.4862],[5009377.0857,3503549.8435],[4452779.6317,4865942.2795]]]}]}";
+       String expected = "{\"type\":\"GeometryCollection\",\"geometries\":[{\"type\":\"Point\",\"coordinates\":[4452779.631730943,1118889.9748579597]}," +
+			   "{\"type\":\"LineString\",\"coordinates\":[[1113194.9079327357,1118889.9748579597],[2226389.8158654715,2273030.926987689],[1113194.9079327357,4865942.279503176]]}," +
+			   "{\"type\":\"Polygon\",\"coordinates\":[[[4452779.631730943,4865942.279503176],[2226389.8158654715,5621521.486192066],[5009377.085697311,3503549.843504374],[4452779.631730943,4865942.279503176]]]}]}";
        assertEquals(expected, geojson);
    }
 }
