82ababe1f362194921abc13cb3ae3496ba6151ff
Toan Tran <khanh.toan.tran@wemove.com>
2023-09-05 11:34:24 +0200

    Try check geometry orientation (#4767)

--- src/main/java/de/ingrid/geo/utils/transformation/WktToGeoJsonTransformUtil.java
+++ src/main/java/de/ingrid/geo/utils/transformation/WktToGeoJsonTransformUtil.java
@@ -28,7 +28,10 @@ import java.io.IOException;
 import org.geotools.geojson.geom.GeometryJSON;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.geometry.jts.WKTReader2;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Polygon;
 import org.geotools.referencing.CRS;
+import org.locationtech.jts.algorithm.Orientation;
 import org.locationtech.jts.io.ParseException;
 import org.opengis.geometry.MismatchedDimensionException;
 import org.opengis.referencing.FactoryException;
@@ -54,12 +57,12 @@ public final class WktToGeoJsonTransformUtil extends WktUtil {
 	 */
 	public static String wktToGeoJson(String wkt) throws ParseException, IOException {
 		WKTReader2 reader = new WKTReader2();
-		org.locationtech.jts.geom.Geometry geometry = reader.read(wkt);
+		Geometry geometry = reader.read(wkt);
 
 		GeometryJSON geometryJSON = new GeometryJSON(10);
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		geometryJSON.write(geometry, out);
-		return out.toString();
+		return checkOrientation(out.toString(), geometry);
 	}
 
 	public static String wktToGeoJsonTransform(String wkt, String dstEpsg) throws ParseException, IOException, MismatchedDimensionException, NoSuchAuthorityCodeException, FactoryException, TransformException {
@@ -68,18 +71,33 @@ public final class WktToGeoJsonTransformUtil extends WktUtil {
 
     public static String wktToGeoJsonTransform(String wkt, String srcEpsg, String dstEpsg) throws ParseException, IOException, NoSuchAuthorityCodeException, FactoryException, MismatchedDimensionException, TransformException {
         WKTReader2 reader = new WKTReader2();
-        org.locationtech.jts.geom.Geometry geometry = reader.read(wkt);
-
+        Geometry geometry = reader.read(wkt);
         CoordinateReferenceSystem sourceCRS = CRS.parseWKT(CoordTransformUtil.getInstance().getWKTString(CoordTransformUtil.getInstance().getCoordTypeByEPSGCode(srcEpsg)));;
         CoordinateReferenceSystem targetCRS = CRS.parseWKT(CoordTransformUtil.getInstance().getWKTString(CoordTransformUtil.getInstance().getCoordTypeByEPSGCode(dstEpsg)));
 
         MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS, true);
         GeometryJSON geometryJSON = new GeometryJSON(10);
-        org.locationtech.jts.geom.Geometry targetGeometry = JTS.transform(geometry, transform);
-        
+        Geometry targetGeometry = JTS.transform(geometry, transform);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         geometryJSON.write(targetGeometry, out);
-        return out.toString();
+        return checkOrientation(out.toString(), targetGeometry);
+    }
+    
+    private static String checkOrientation(String json, Geometry geom) {
+     // Check polygon orientation
+        switch (geom.getGeometryType()) {
+        case Polygon.TYPENAME_POLYGON:
+            Polygon pol = (Polygon) geom;
+            if(Orientation.isCCW(geom.getCoordinates()) && pol.getNumInteriorRing() == 0) {
+                if(json.indexOf(",\"coordinates\":") > -1) {
+                    json = json.replace(",\"coordinates\":", ",\"orientation\":\"LEFT\",\"coordinates\":");
+                }
+            }
+            break;
+        default:
+            break;
+        }
+        return json;
     }
 }
 
